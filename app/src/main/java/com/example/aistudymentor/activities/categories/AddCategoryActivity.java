9d5b8683165013c9cd4853fd658a2f932dce6d0b
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2026-07-17 10:12:58 +0700

    update code

--- app/src/main/java/com/example/aistudymentor/activities/categories/AddCategoryActivity.java
+++ app/src/main/java/com/example/aistudymentor/activities/categories/AddCategoryActivity.java
@@ -1,20 +1,26 @@
 package com.example.aistudymentor.activities.categories;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.aistudymentor.R;
 import com.example.aistudymentor.activities.MenuActivity;
+import com.example.aistudymentor.repository.CategoryRepository;
 
 public class AddCategoryActivity extends AppCompatActivity {
     EditText edtCategoryName, edtDescription;
     Button btnSave, btnBack;
+    CategoryRepository categoryRepository;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,34 @@ public class AddCategoryActivity extends AppCompatActivity {
         edtDescription  = findViewById(R.id.edtCategoryDescription);
         btnSave = findViewById(R.id.btnSaveNewCategory);
         btnBack = findViewById(R.id.btnBackNewCategory);
+        categoryRepository = new CategoryRepository(AddCategoryActivity.this);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                String categoryName = edtCategoryName.getText().toString().trim();
+                if (TextUtils.isEmpty(categoryName)) {
+                    edtCategoryName.setError("Category name is required");
+                    return;
+                }
+                String description = edtDescription.getText().toString().trim();
+                long insert = categoryRepository.saveNewCategory(categoryName, description);
+                if (insert == -1){
+                    // co loi xay ra, khong them duoc du lieu
+                    Toast.makeText(AddCategoryActivity.this, "Created Fail", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // thanh cong
+                Toast.makeText(AddCategoryActivity.this, "Created Successfully", Toast.LENGTH_SHORT).show();
+                // quay ve lai trang menu
+                Intent intent = new Intent(AddCategoryActivity.this, MenuActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("MENU_TAB","category");
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/aistudymentor/repository/CategoryRepository.java
+++ app/src/main/java/com/example/aistudymentor/repository/CategoryRepository.java
@@ -1,4 +1,31 @@
 package com.example.aistudymentor.repository;
 
-public class CategoryRepository {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import com.example.aistudymentor.helpers.Helper;
+import com.example.aistudymentor.sqlite.DbHelper;
+
+public class CategoryRepository extends DbHelper {
+    public CategoryRepository(@Nullable Context context) {
+        super(context);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public long saveNewCategory(String name, String description){
+        String currentDate = Helper.getCurrentDate();
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(NAME_CATEGORY, name);
+        values.put(DESCRIPTION_CATEGORY, description);
+        values.put(STATUS_CATEGORY, 1);
+        values.put(CREATED_AT, currentDate);
+        long insert = db.insert(CATEGORY_TABLE, null, values);
+        db.close();
+        return insert;
+    }
 }
